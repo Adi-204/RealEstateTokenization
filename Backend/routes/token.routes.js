fbae789e2f13f3d26a3549eb3404d0c32a5e8b18
Adi-204 <adiboghawala@gmail.com>
2025-03-24 18:10:22 +0530

    feat: changes in contract model and added routes

--- Backend/routes/token.routes.js
+++ Backend/routes/token.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getTokens, addTokens, delTokens } = require("../controllers/token.controller.js");
+const { getTokens, addTokens, delTokens, getAllTokens, setLocation } = require("../controllers/token.controller.js");
 
 const router = express.Router();
 
 router.route("/get").get(getTokens);
+router.route("/getall").get(getAllTokens);
+router.route("/setlocation").post(setLocation);
 router.route("/add").post(addTokens);
 router.route("/del").post(delTokens);
 
